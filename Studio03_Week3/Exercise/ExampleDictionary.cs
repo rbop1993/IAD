bcf379436178afe611e4c3fde8af89dbd1836eca
Rajat Bopardikar <rajatbopardikar@outlook.com>
2019-08-22 22:38:10 +1000

    Studio03_Week03 pushed to git.

---
+++ Studio03_Week3/Exercise/ExampleDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Studio03_Week3.Exercise
+{
+    public class ExampleDictionary
+    {
+        public void Example()
+        {
+            Dictionary<int, Student> studentDictionary = new Dictionary<int, Student>();
+            Student s1 = new Student("Alex", "0431170991");
+            Student s2 = new Student("Kim", "0431170992");
+            studentDictionary.Add(1, s1);
+            studentDictionary.Add(2, s2);
+            Student result = new Student("", "");
+            studentDictionary.TryGetValue(1, out result);
+        }
+    }
+}
